119da6eb53824bae85dfbbbc6e808ed9d9afab23
Aitor Riba Archilla <contact@aitorriba.com>
2017-10-09 23:06:32 +0200

    Update Link.php

--- src/Models/Link.php
+++ src/Models/Link.php
@@ -198,11 +198,11 @@ class Link extends Model
     public function getIP()
     {
         if (! empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {   //check ip from cloudflare
-          $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+            $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
         } elseif (! empty($_SERVER['HTTP_CLIENT_IP'])) {   //check ip from share internet
-          $ip = $_SERVER['HTTP_CLIENT_IP'];
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
         } elseif (! empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {   //to check ip is pass from proxy
-          $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
         } else {
             $ip = $_SERVER['REMOTE_ADDR'];
         }
